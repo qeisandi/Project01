18474ec4b2f604b107ff83c14534750edce93f3c
qeis andi <qeis.andi02@gmail.com>
2025-08-11 22:44:07 +0700

    Remove App.tsx, update NDK version in build.gradle, and add VSCode settings

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { View,Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style= {{textAlign: 'center',fontSize: 20}}>Hello World</Text>
+      <Text>Deskripsi</Text>
+    </View>
+  );
+}
+
+export default App;
